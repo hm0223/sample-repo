8d82b4b6283d23110ac30a438074faf9009ead13
huwenfeng <809261665@qq.com>
2023-12-01 14:36:43 +0800

    feat: junit5 suit samples.

---
+++ junit5-samples/src/test/java/com/hm/junit5/samples/biz/biz1/Biz1Test.java
@@ -0,0 +1,19 @@
+package com.hm.junit5.samples.biz.biz1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Biz1Test.
+ *
+ * @author huwenfeng
+ */
+class Biz1Test {
+
+    @Test
+    @DisplayName("test biz 1")
+    void testBiz1() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ junit5-samples/src/test/java/com/hm/junit5/samples/biz/biz2/Biz2Test.java
@@ -0,0 +1,19 @@
+package com.hm.junit5.samples.biz.biz2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Biz2Test.
+ *
+ * @author huwenfeng
+ */
+class Biz2Test {
+
+    @Test
+    @DisplayName("test biz 2")
+    void testBiz2() {
+        Assertions.assertTrue(true);
+    }
+}
